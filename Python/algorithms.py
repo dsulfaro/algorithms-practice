d0a027c6e9cd6ed8fa9c617fbaff1000e3ccfb27
Dan Sulfaro <dsulfaro1@gmail.com>
2017-01-11 15:58:03 -0800

    finish common_substrings

--- Python/algorithms.py
+++ Python/algorithms.py
@@ -8,3 +8,17 @@ def caesar_cipher(string, shift):
         num = (num + shift) % 26
         result += chr(num + 96)
     return result
+
+def common_substrings(s, t):
+    result = ""
+    if (len(s) > len(t)):
+        s, t = t, s
+    for i, val in enumerate(s):
+        for j, xal in enumerate(s):
+            if j <= i:
+                continue
+            else:
+                sub = s[i:j]
+                if (sub in t) and (len(sub) > len(result)):
+                    result = sub
+    return result
