2a9c94b01b8ad7c3d11ea60fc8ea6cdadd513531
Dan Sulfaro <dsulfaro1@gmail.com>
2017-01-25 17:37:42 -0800

    finish one_away

--- CTCI/Python/arrays_strings.py
+++ CTCI/Python/arrays_strings.py
@@ -1,3 +1,44 @@
+def replace_character_check(s1, s2):
+    i = 0
+    change = False
+    while i < len(s1):
+        if s1[i] != s2[i]:
+            if change:
+                return False
+            else:
+                change = True
+        i += 1
+    return True
+
+def one_extra_character_checks(s1, s2):
+    # make s1 the longer string
+    if len(s2) > len(s1):
+        s1, s2 = s2, s1
+    i = 0
+    j = 0
+    change = False
+    while i < len(s1) and j < len(s2):
+        if s1[i] == s2[j]:
+            i += 1
+            j += 1
+        else:
+            if change:
+                return False
+            else:
+                change = True
+                j -= 1
+            i += 1
+            j += 1
+    return True
+
+def one_away(s1, s2):
+    if abs(len(s1) - len(s2)) > 1:
+        return False
+    if len(s1) == len(s2):
+        return replace_character_check(s1, s2)
+    else:
+        return one_extra_character_checks(s1, s2)
+
 def string_compression(s):
     result = ""
     i = 0
